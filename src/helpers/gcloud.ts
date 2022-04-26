5c8fac6a3d8cb927dab1fc2134ff20a74eb6abf0
Andy <andreas.clausen@hotmail.com>
2022-04-26 17:31:14 +0200

    fix: properly throw errors from gcloud

--- src/helpers/gcloud.ts
+++ src/helpers/gcloud.ts
@@ -1,10 +1,13 @@
 import { Configuration, execUtils } from '@yarnpkg/core';
 
 export async function gcloud(configuration: Configuration, ...args: string[]): Promise<string> {
-  const { stdout } = await execUtils.execvp('gcloud', args, {
+  const { stdout, stderr } = await execUtils.execvp('gcloud', args, {
     cwd: configuration.projectCwd,
     encoding: 'utf-8',
   });
+  if (stderr) {
+    throw stderr;
+  }
 
   return stdout;
 }
